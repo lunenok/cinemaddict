abb4b2dbeb702b3f21c4a25d091260876f53c126
Maksim <familinov@gmail.com>
2020-04-14 20:35:08 +0500

    Личный проект: время разбивать камни

---
+++ src/components/most-commented-list.js
@@ -0,0 +1,10 @@
+export const createMostCommentedFilmsListTemplate = () => {
+  return (
+    `<section class="films-list--extra films-list--commented">
+      <h2 class="films-list__title">Most commented</h2>
+      <div class="films-list__container">
+      </div>
+    </section>
+    `
+  );
+};
---
+++ src/components/top-rated-list.js
@@ -0,0 +1,9 @@
+export const createTopRatedFilmsListTemplate = () => {
+  return (
+    `<section class="films-list--extra films-list--top">
+      <h2 class="films-list__title">Top rated</h2>
+      <div class="films-list__container">
+      </div>
+    </section>`
+  );
+};
