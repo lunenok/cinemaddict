5cca3c85a659b4d76ffdf5b4270584d5af9b247a
lunenok <familinov@gmail.com>
2020-05-16 22:42:42 +0500

    render

--- src/main.js
+++ src/main.js
@@ -10,7 +10,8 @@ import ShowMoreButtonComponent from "./components/show-more-button.js";
 import FilmDetailsComponent from "./components/film-details.js";
 import NoFilmsComponent from "./components/no-films.js";
 import {generateMovies} from "./mock/mock.js";
-import {calculateFilters, render, RenderPosition} from "./utils.js";
+import {calculateFilters} from "./utils/common.js";
+import {render, RenderPosition, remove} from "./utils/render.js";
 
 export const Movies = generateMovies(15);
 const filtersCount = calculateFilters(Movies);
@@ -22,55 +23,22 @@ const MOST_COMMENTED_MOVIE_CARD_COUNT = 2;
 // Рендер управления и главной доски
 const siteHeaderElement = document.querySelector(`header`);
 const siteMainElement = document.querySelector(`.main`);
-render(siteHeaderElement, new ProfileComponent().getElement(), RenderPosition.BEFOREEND);
-render(siteMainElement, new MenuComponent(filtersCount).getElement(), RenderPosition.BEFOREEND);
-render(siteMainElement, new SortingComponent().getElement(), RenderPosition.BEFOREEND);
-render(siteMainElement, new MainBoardComponent().getElement(), RenderPosition.BEFOREEND);
-
-const renderMovie = (listComponent, movie) => {
-  const filmCardComponent = new FilmCardComponent(movie);
-  const movieCardPoster = filmCardComponent.getElement().querySelector(`.film-card__poster`);
-
-  const filmDetailsComponent = new FilmDetailsComponent(movie);
-
-  const onCloseButtonClick = () => {
-    filmDetailsComponent.getElement().remove();
-    filmDetailsComponent.removeElement();
-  };
-
-  const onMovieCardClick = () => {
-    const closeButton = filmDetailsComponent.getElement().querySelector(`.film-details__close`);
-    render(siteMainElement, filmDetailsComponent.getElement(), RenderPosition.BEFOREEND);
-    document.addEventListener(`keydown`, onEscKeyDown);
-    closeButton.addEventListener(`click`, onCloseButtonClick);
-  };
-
-  const onEscKeyDown = (evt) => {
-    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscKey) {
-      filmDetailsComponent.getElement().remove();
-      filmDetailsComponent.removeElement();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  movieCardPoster.addEventListener(`click`, onMovieCardClick);
-
-  render(listComponent, filmCardComponent.getElement(), RenderPosition.BEFOREEND);
-};
+render(siteHeaderElement, new ProfileComponent(), RenderPosition.BEFOREEND);
+render(siteMainElement, new MenuComponent(filtersCount), RenderPosition.BEFOREEND);
+render(siteMainElement, new SortingComponent(), RenderPosition.BEFOREEND);
+render(siteMainElement, new MainBoardComponent(), RenderPosition.BEFOREEND);
 
 const renderBoard = (movies) => {
   const mainBoardElement = document.querySelector(`.films`);
 
   if (movies.length === 0) {
-    render(mainBoardElement, new NoFilmsComponent().getElement(), RenderPosition.AFTERBEGIN);
+    render(mainBoardElement, new NoFilmsComponent(), RenderPosition.AFTERBEGIN);
     return;
   }
 
-  render(mainBoardElement, new FilmsListComponent().getElement(), RenderPosition.BEFOREEND); // Не рендерит
-  render(mainBoardElement, new TopRatedFilmsListComponent().getElement(), RenderPosition.BEFOREEND);
-  render(mainBoardElement, new MostCommentedFilmsListComponent().getElement(), RenderPosition.BEFOREEND);
+  render(mainBoardElement, new FilmsListComponent(), RenderPosition.BEFOREEND); // Не рендерит
+  render(mainBoardElement, new TopRatedFilmsListComponent(), RenderPosition.BEFOREEND);
+  render(mainBoardElement, new MostCommentedFilmsListComponent(), RenderPosition.BEFOREEND);
 
   const filmsListElement = document.querySelector(`.films-list`);
   const filmsContainerElement = filmsListElement.querySelector(`.films-list__container`);
@@ -82,11 +50,12 @@ const renderBoard = (movies) => {
   movies.slice(0, showingMoviesCount)
     .forEach((movie) => renderMovie(filmsContainerElement, movie));
 
-  render(filmsListElement, new ShowMoreButtonComponent().getElement(), RenderPosition.BEFOREEND);
+  const showMoreButtonComponent = new ShowMoreButtonComponent();
+  render(filmsListElement, showMoreButtonComponent, RenderPosition.BEFOREEND);
 
   const showMoreButton = filmsListElement.querySelector(`.films-list__show-more`);
 
-  showMoreButton.addEventListener(`click`, () => {
+  showMoreButtonComponent.setClickHandler(() => {
     const prevMoviesCount = showingMoviesCount;
     showingMoviesCount = showingMoviesCount + MOVIE_CARD_COUNT;
 
